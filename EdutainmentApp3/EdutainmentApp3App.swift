61c32c17b1561e4ecef26191cdbfb77170a441d1
Lin <linochoa11690@gmail.com>
2024-11-23 21:02:19 -0500

    Initial Commit

---
+++ EdutainmentApp3/EdutainmentApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp3App.swift
+//  EdutainmentApp3
+//
+//  Created by Lin Ochoa on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EdutainmentApp3Tests/EdutainmentApp3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp3Tests.swift
+//  EdutainmentApp3Tests
+//
+//  Created by Lin Ochoa on 11/23/24.
+//
+
+import Testing
+@testable import EdutainmentApp3
+
+struct EdutainmentApp3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
